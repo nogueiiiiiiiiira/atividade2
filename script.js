1d49935dd50091f95271d544b47c89591fee4619
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-02-15 08:53:40 -0300

    Atividade

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 var nomeGlobal;
 var mensagemGlobal;
 
-function Conferir(){
-    var nome = document.getElementById("nome").value
-    var mensagem = document.getElementById("mensagem").value
+function Conferir() {
+    var nome = document.getElementById("nome").value;
+    var mensagem = document.getElementById("mensagem").value;
 
     nomeGlobal = nome;
     mensagemGlobal = mensagem;
 
-    document.getElementById("ConfirmeNome").textContent = nome;
-    document.getElementById("ConfirmeMens").textContent = mensagem;
+    document.getElementById("ConfirmeNome").value = nome;
+    document.getElementById("ConfirmeMens").value = mensagem;
 }
 
-function enviar(){
+function enviar() {
     var numeroTelefone = "554195340694";
     var linkWhats = "https://wa.me/" + numeroTelefone + "?text=NOME: " + nomeGlobal + " - " + mensagemGlobal;
 
-    window.open(linkWhats, "_blank")
-    }
+    window.open(linkWhats, "_blank");
+}
+
+function limpar(){
+    window.location.reload();
+}
